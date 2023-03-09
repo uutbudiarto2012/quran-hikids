d9e0273d3f5a1f7a97c8ba2e1592492be121c21e
Uut Budiarto <uutbudiarto@Uuts-MacBook-Pro.local>
2023-03-09 20:32:23 +0700

    update pwa 1

--- next.config.js
+++ next.config.js
@@ -1,42 +1,4 @@
 /** @type {import('next').NextConfig} */
-// const withPWA = require('next-pwa')({
-//   reactStrictMode: true,
-//   env: {
-//     api: process.env.APIURL,
-//   },
-//   dest: 'public',
-//   disable: process.env.NODE_ENV === 'development',
-//   register: true,
-//   scope: '/app',
-//   sw: 'service-worker.js'
-// })
-// const nextConfig = {
-//   reactStrictMode: true,
-//   env: {
-//     api: process.env.APIURL,
-//   },
-// }
-
-
-// module.exports = withPWA
-// const withPWA = require("next-pwa");
-// module.exports = withPWA({
-//   reactStrictMode: true,
-//   env: {
-//     api: process.env.APIURL,
-//   },
-//   pwa: {
-//     dest: "public",
-//     register: true,
-//     skipWaiting: true,
-//     disable: process.env.NODE_ENV === 'development',
-//     register: true,
-//     scope: '/',
-//     sw: 'service-worker.js'
-//   },
-// });
-
-
 const runtimeCaching = require("next-pwa/cache");
 const withPWA = require("next-pwa")({
   dest: "public",
--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,39 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="application-name" content="Quran HiKids" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Quran HiKids" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-config" content="/icon-192x192.png" />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#0f172a" />
+
+        <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icon-192x192.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icon-192x192.png" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/icon-192x192.png" />
+
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="mask-icon" href="/icons/icon-192x192.png" color="#0f172a" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:url" content="https://quran-hkids.com" />
+        <meta name="twitter:title" content="Quran HiKids" />
+        <meta name="twitter:description" content="Best Quran HiKids in the world" />
+        <meta name="twitter:image" content="https://quran-hkids.com/icon-192x192.png" />
+        <meta name="twitter:creator" content="@DavidWShadow" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Quran HiKids" />
+        <meta property="og:description" content="Best Quran HiKids in the world" />
+        <meta property="og:site_name" content="Quran HiKids" />
+        <meta property="og:url" content="https://quran-hkids.com" />
+        <meta property="og:image" content="https://quran-hkids.com/icon-192x192.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
