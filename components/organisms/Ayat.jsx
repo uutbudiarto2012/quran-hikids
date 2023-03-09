726366ac9adbb1e8b2b5e4e5fa217a257615e969
Uut Budiarto <uutbudiarto@Uuts-MacBook-Pro.local>
2023-03-09 14:09:50 +0700

    add audio player

--- components/organisms/Ayat.jsx
+++ components/organisms/Ayat.jsx
@@ -5,6 +5,8 @@ import useSWR from 'swr'
 import utils from '../../data/utils.json'
 import AyatCard from '../molecules/Cards/AyatCard'
 import LoaderAyat from '../molecules/Skeleton/LoaderAyat'
+import ReactAudioPlayer from 'react-audio-player'
+import 'react-audio-player-pro/dist/style.css'
 
 async function fetcher (url) {
   const response = await axios({
@@ -19,7 +21,7 @@ const Ayat = () => {
   const { data, isLoading } = useSWR(URL, fetcher)
   return (
     <>
-      <div className='flex mt-2 sticky top-[55px] md:top-[65px] bg-slate-50 py-2 justify-between'>
+      <div className='flex mt-2 sticky z-30 top-[55px] md:top-[65px] bg-slate-50 py-2 justify-between'>
         <button
           onClick={() => router.back()}
           className='flex gap-1 text-sm items-center'
@@ -28,7 +30,13 @@ const Ayat = () => {
         </button>
         <div className='text-end text-xl font-semibold'>{utils.basmalah}</div>
       </div>
-
+      <div className='my-3'>
+        <ReactAudioPlayer
+          className='bg-transparent w-full'
+          src={data?.audio}
+          controls
+        />
+      </div>
       {isLoading && <LoaderAyat count={12} />}
       {data?.ayat?.map(item => (
         <AyatCard
